4ca69fa3706f90b083aacf580c1ca9a5df8b64c6
Srikanth Suresh <4140019+srikwit@users.noreply.github.com>
2019-07-24 00:06:05 +0530

    Added buggy pascal triangle and updated Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,22 @@ pub fn factorial(n: u32) -> u32{
     }
 }
 
+pub fn pascal(c: u32, r: u32) -> u32{
+
+    fn inner_checked_addition(a: u32, b: u32) -> u32 {
+        match a.checked_add(b){
+            Some(v) => v,
+            None => 0
+        }
+    }
+
+    match (c,r) {
+    (c,r) if c == 0 || c == r => 1,
+    (c,r) if c == r - 1 => inner_checked_addition(1 , pascal(c - 1 , r - 1)),
+     _ => match inner_checked_addition(pascal(c, r -1) , pascal(c - 1, r - 1)) { x  if x > 0 => x, _ => 0 }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -24,10 +40,25 @@ mod tests {
     }
     #[test]
     fn test_factorial_zero_and_one(){
-        assert_eq!(factorial(1), 1);                                                                                                                                                                    assert_eq!(factorial(0), 1);
+         assert_eq!(factorial(1), 1);
+         assert_eq!(factorial(0), 1);
     }
     #[test]
-    fn test_large_numbers(){
+    fn test_factorial_large_numbers(){
         assert_eq!(factorial(u32::max_value()-1),0);
+    }
+
+    #[test]
+    fn test_pascal_small_positive_integers(){
+        assert_eq!(pascal(1,3),3);
+        assert_eq!(pascal(1,2),2);
+        assert_eq!(pascal(0,2),1);
+    }
+   
+    //Failed attempt at preventing overflow for large values in pascal triangle. TODO: Handle
+    //overflow gracefully
+    #[test]
+    fn test_pascal_large_numbers(){
+        assert_eq!(pascal(u32::max_value()/4,u32::max_value()/2),0);
     }
 }
